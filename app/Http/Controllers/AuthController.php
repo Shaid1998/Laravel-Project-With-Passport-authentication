43ae8c7c57a001c9453e87c77549b44a2d8366dc
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-08-09 15:59:59 +0600

    created nessacary request and controller and middleware

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,53 +5,74 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use App\Models\User;
+
+use App\Http\Requests\RegiserRequest;
 use DB;
-Use Illuminate\Support\Facades\Hash;
-use App\Http\Requests\RegisterRequest;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+
+
     public function Login(Request $request){
-        try{
-            if(Auth::attempt($request->only('email','password'))){
-                $user = Auth::user();
-                $token = user->createToken('app')->accessToken;
 
-                return response([
-                    'messege' => 'Successfully Login',
-                    'token' => $token,
-                    'user' => $user
-                ],200);//states codes
-            }
-        }catch(Exception $e){
-            return response([
-                'message' => $exception->getMessage()
-            ],400);//states codes
-        }
-        return response([
-            'messege' => 'Invalid Email or Password'
-        ],401);
+    	try{
 
-    }//End Method
+    		if (Auth::attempt($request->only('email','password'))) {
+    			$user = Auth::user();
+    			$token = $user->createToken('app')->accessToken;
 
-    public function Register(RegisterRequest $request){
-        try{
-            $user = User::create([
-                'name' =>$request->name,
-                'email' => $request->email,
-                'password' => Hash::make($request->password)
-            ]);
-            $token = $user->$createToken('app')->accessToken;
-            return response([
-                'messege' => 'Registration Login',
-                'token' => $token,
-                'user' => $user
-            ],200);//states codes
+    			return response([
+    				'message' => "Successfully Login",
+    				'token' => $token,
+    				'user' => $user
+    			],200); // States Code
+    		}
 
-        }catch(Exception $e){
-            return response([
-                'message' => $exception->getMessage()
-            ],400);//states codes
-        }
-    }
+    	}catch(Exception $exception){
+    		return response([
+    			'message' => $exception->getMessage()
+    		],400);
+    	}
+    	return response([
+    		'message' => 'Invalid Email Or Password' 
+    	],401);
+
+    } // end method 
+
+
+
+    public function Register(RegiserRequest $request){
+
+    	try{
+
+    		$user = User::create([
+    			'name' => $request->name,
+    			'email' => $request->email,
+    			'password' => Hash::make($request->password) 
+    		]);
+    		$token = $user->createToken('app')->accessToken;
+
+    		return response([
+    			'message' => "Registration Successfull",
+    			'token' => $token,
+    			'user' => $user
+    		],200);
+
+	    	}catch(Exception $exception){
+	    		return response([
+	    			'message' => $exception->getMessage()
+	    		],400);
+	    	}
+
+
+
+
+    } // end mehtod 
+
+
+
+
+
 }
+ 
